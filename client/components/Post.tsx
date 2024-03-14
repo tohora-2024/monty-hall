7a7603b2e40af404468068ee8f169ac226115fdd
Tyler McBride <imtylermcbride@gmail.com>
2024-03-15 07:44:54 +1300

    post component written

--- client/components/Post.tsx
+++ client/components/Post.tsx
@@ -1,3 +1,12 @@
-export default function Post() {
-  return <></>
+import { MessageData } from '../models/dogimages'
+
+export default function Post(post: MessageData) {
+  console.log(post.author)
+  return (
+    <div>
+      <p>{post.message}</p>
+      <p>{post.author}</p>
+      <img src={post.photoURL} alt="generated author profile pic from an API" />
+    </div>
+  )
 }
