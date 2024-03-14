4e18d51d60939a0b89a7b7c66e6e23ea7455a289
Tyler McBride <imtylermcbride@gmail.com>
2024-03-14 21:38:21 +1300

    Submission form taking and compiling message post

--- client/components/SubmissionForm.tsx
+++ client/components/SubmissionForm.tsx
@@ -1,4 +1,30 @@
+import React, { useState } from 'react'
+
 export default function SubmissionForm() {
+  const [postInfo, setPostInfo] = useState({
+    author: '',
+    message: '',
+  })
+
+  const handleChange = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = event.currentTarget
+    setPostInfo((prev) => ({ ...prev, [name]: value }))
+  }
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event?.preventDefault()
+    console.log(postInfo)
+
+    // --- SEMI PSEUDOCODE FOR Message Post Hook Call ---
+    // const message = { ...postInfo}
+
+    // if (message && typeof message === 'object'){
+    //   usePostToDatabase(information)
+    // }
+  }
+
   return (
     <>
       <h1>Submission form</h1>
@@ -6,14 +32,31 @@ export default function SubmissionForm() {
         <p>Hi there, please leave a kind message</p>
       </div>
       <span>
-        <form>
-          <input></input>
-          <input></input>
-          <select>
+        <form onSubmit={handleSubmit}>
+          <input
+            onChange={handleChange}
+            type="text"
+            placeholder="Name"
+            id="Author"
+            name="author"
+            value={postInfo.author}
+          ></input>
+          <div>
+            <textarea
+              onChange={handleChange}
+              placeholder="Message"
+              maxLength={200}
+              id="Message"
+              name="message"
+              value={postInfo.message}
+            ></textarea>
+          </div>
+          {/* This is a stretch opportunity for if we are allowing people to randomise their picture from different sources */}
+          {/* <select>
             <option value="dog">Dog</option>
             <option value="cat">Cat</option>
-          </select>
-          <button>Submit</button>
+          </select> */}
+          <button type="submit">Submit</button>
         </form>
       </span>
     </>
