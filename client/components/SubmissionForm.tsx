8aa83bfd4fa4c6f4fe720ab9068db16407d75157
Tyler McBride <imtylermcbride@gmail.com>
2024-03-14 16:35:24 +1300

    progress save

--- client/components/SubmissionForm.tsx
+++ client/components/SubmissionForm.tsx
@@ -1,7 +1,21 @@
 export default function SubmissionForm() {
   return (
     <>
-      <h1>submission form</h1>
+      <h1>Submission form</h1>
+      <div>
+        <p>Hi there, please leave a kind message</p>
+      </div>
+      <span>
+        <form>
+          <input></input>
+          <input></input>
+          <select>
+            <option value="dog">Dog</option>
+            <option value="cat">Cat</option>
+          </select>
+          <button>Submit</button>
+        </form>
+      </span>
     </>
   )
 }
